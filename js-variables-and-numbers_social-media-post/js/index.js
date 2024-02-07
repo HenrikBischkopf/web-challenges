624657608c12dde7d4def0b9fd97aea547764df8
Henrik <henrik@bischkopf.de>
2024-02-07 12:23:36 +0100

    added new js challenges

---
+++ js-variables-and-numbers_social-media-post/js/index.js
@@ -0,0 +1,33 @@
+console.clear();
+
+/*
+1. Create the data for a small social media post. Create a const variable for each data point listed below.:
+	- a title
+	- a text
+	- the number of likes
+	- the user who created the post
+	- a boolean called isReported
+*/
+
+// --v-- write your code here --v--
+let title = "Social Media Post";
+let text =
+  "Welcome to this exercise! Please open the console before you start.";
+let numberLikes = 0;
+let userPost = "Name";
+let isReported = true;
+// --^-- write your code here --^--
+
+/*
+2. Log all variables to the console. Increase the likes by one and log the amount of likes again. Adapt your code from 1.1 if necessary.
+*/
+
+// --v-- write your code here --v--
+console.log(title);
+console.log(text);
+console.log(numberLikes);
+console.log(userPost);
+console.log(isReported);
+
+console.log(numberLikes + 1);
+// --^-- write your code here --^--
