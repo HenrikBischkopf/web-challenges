9b8a453544bd873d5f915811885778bdd1524f3e
Henrik <henrik@bischkopf.de>
2024-03-18 16:54:26 +0100

    working on the Tourio App

---
+++ recap-project-6_tourio-app/pages/api/places/[id]/index.js
@@ -0,0 +1,21 @@
+//import { places } from "../../../../lib/db.js";
+import dbConnect from "@/db/connect";
+import Place from "@/db/models/Place";
+
+export default async function handler(request, response) {
+  await dbConnect();
+  const { id } = request.query;
+
+  if (!id) {
+    return;
+  }
+  if (request.method === "GET") {
+    const place = await Place.findById(id);
+
+    if (!place) {
+      return response.status(404).json({ status: "Not found" });
+    }
+
+    response.status(200).json(place);
+  }
+}
